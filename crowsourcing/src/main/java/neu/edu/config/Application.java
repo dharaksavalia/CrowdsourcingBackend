e8c0306223feac104b955c263dcc0d51849bd923
Dharak <Dharak@DESKTOP-11PCMMV>
2017-12-10 11:17:55 -0500

    initial configuration

---
+++ crowsourcing/src/main/java/neu/edu/config/Application.java
@@ -0,0 +1,33 @@
+package neu.edu.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@ComponentScan({"neu.edu.controller","neu.edu.service"})
+@EnableJpaRepositories("neu.edu.dao")
+@EntityScan("neu.edu.entity")
+public class Application implements CommandLineRunner{
+	
+	@Autowired
+	private DataSource dataSource;
+	
+	public static void main(String...args){
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		  System.out.println("DATASOURCE = " + dataSource);
+		
+	}
+
+}
